d906e5d57eaab81bed43c645190dfdd01775efc4
Ana <anadecanha@gmail.com>
2024-05-15 09:45:13 +0900

    prepare for testing

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,9 +1,20 @@
-import { screen, render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
 import Home from "../app/page";
 
 describe("Home", () => {
   render(<Home />);
-  it("should render HomePage", () => {
-    screen.getByText("app/page.tsx");
+  const startButton = screen.getByRole("button");
+
+  it("should render disable button when clicked and show loading state", async () => {
+    await userEvent.click(startButton);
+    expect(startButton).toBeDisabled();
+    expect(screen.getByText("Loading")).toBeInTheDocument;
+  });
+
+  it("should show a list of headlines news", async () => {
+    // Should I do this separate??
   });
 });
