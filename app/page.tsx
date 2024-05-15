a86d3d1968972fa819a0842b998224bdad1ff36e
Ana <anadecanha@gmail.com>
2024-05-15 11:13:19 +0900

    add new components, add config to make api call"

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import useApi from "@/hooks/useApi";
+
+interface YourDataType {
+  // Define the structure of your data here
+  id: number;
+  name: string;
+  // Other fields...
+}
+
 export default function Home() {
-  return <button>Start</button>;
+  const url =
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=579700442f3b4257878e1d4cee6e714d";
+  // useApi(url);
+  const { data, error, loading } = useApi<YourDataType[]>(url);
+
+  return (
+    <>
+      <Button>Start</Button>
+      {data && <Card />}
+    </>
+  );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function fetcher<T>(
+  url: string,
+  options: RequestInit = {}
+): Promise<T> {
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  console.log("RESPONSE ->", response);
+  return response.json() as Promise<T>;
 }
