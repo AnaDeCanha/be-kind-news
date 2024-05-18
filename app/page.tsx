3e0594d3b8a72d351d234515cfa1e40157210d3f
Ana <anadecanha@gmail.com>
2024-05-18 12:17:59 +0900

    add swr, make api call

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,21 @@
 
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import useApi from "@/hooks/useApi";
-
-interface YourDataType {
-  // Define the structure of your data here
-  id: number;
-  name: string;
-  // Other fields...
-}
+import useSWR from "swr";
 
 export default function Home() {
-  const url =
+  const URL =
     "https://newsapi.org/v2/top-headlines?country=us&apiKey=579700442f3b4257878e1d4cee6e714d";
-  // useApi(url);
-  const { data, error, loading } = useApi<YourDataType[]>(url);
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(URL, fetcher);
+
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
     <>
       <Button>Start</Button>
-      {data && <Card />}
+      {data && <Card>ARTICLES</Card>}
     </>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,15 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export async function fetcher<T>(
-  url: string,
-  options: RequestInit = {}
-): Promise<T> {
-  const response = await fetch(url, options);
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  console.log("RESPONSE ->", response);
-  return response.json() as Promise<T>;
-}
