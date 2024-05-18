e7bba66b9dfaf6d9efef4d8c957ca74a8b473200
Ana <anadecanha@gmail.com>
2024-05-18 13:44:38 +0900

    create a state and save the datawith useEffect, create list, show only the max content

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,64 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import useSWR from "swr";
 
+interface Source {
+  id: string | null;
+  name: string;
+}
+
+interface Article {
+  source: Source;
+  author: string | null;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string | null;
+}
+
+interface ApiResponse {
+  status: string;
+  totalResults: number;
+  articles: Article[];
+}
+
+const URL: String =
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=579700442f3b4257878e1d4cee6e714d";
+const MAX_CONTENT: number = 5;
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 export default function Home() {
-  const URL =
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=579700442f3b4257878e1d4cee6e714d";
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR(URL, fetcher);
+  const { data, error } = useSWR<ApiResponse>(URL, fetcher);
+  const [articles, setArticles] = useState<Article[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      setArticles(data.articles);
+    }
+  }, [data]);
+
+  const listHeadlines = articles.slice(0, MAX_CONTENT).map((article) => {
+    return (
+      <li key={article.source.id || article.title}>
+        <Card>
+          <CardHeader>
+            <CardTitle>{article.title}</CardTitle>
+            <CardDescription>{article.description}</CardDescription>
+          </CardHeader>
+        </Card>
+      </li>
+    );
+  });
 
   if (error) return <div>Error: {error.message}</div>;
   if (!data) return <div>Loading...</div>;
@@ -16,7 +66,7 @@ export default function Home() {
   return (
     <>
       <Button>Start</Button>
-      {data && <Card>ARTICLES</Card>}
+      <ul>{listHeadlines}</ul>
     </>
   );
 }
